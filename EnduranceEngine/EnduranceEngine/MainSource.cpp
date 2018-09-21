e0b73684b221dd264a969d4bc05adc45992b88f6
BombardierBrisk <aljonjake45@gmail.com>
2018-09-21 14:05:41 -0400

    Lab 2 Submission
    + Check Instance
    + Check Storage
    + Check Memory
    + Read CPU Speed

--- EnduranceEngine/EnduranceEngine/MainSource.cpp
+++ EnduranceEngine/EnduranceEngine/MainSource.cpp
@@ -12,26 +12,22 @@ using namespace std;
 using namespace DirectX;
 using namespace DirectX::PackedVector;
 
-
-
-int main() {
+bool CheckInstance()
+{
 
 	CreateMutex(NULL, TRUE, "MyMutex");
 	if (GetLastError() == ERROR_ALREADY_EXISTS)
 	{
 
-		cout << "Stup";
+		cout << "An instance is already running!" << endl;
 		system("pause");
-		return FALSE;
+		return false;
 
 
 	}
 
-
-
-	cout << "asda";
-	system("pause");
-
+	cout << "No other instances are running." << endl;
+	return true;
 }
 
 bool CheckStorage(const DWORDLONG diskSpaceNeeded)
@@ -45,10 +41,74 @@ bool CheckStorage(const DWORDLONG diskSpaceNeeded)
 
 	if (diskfree.avail_clusters < neededClusters)
 	{
-		cout << "check";
+		cout << "You don't have enough disk space!" << endl;
+		system("pause");
 		return false;
 
 	}
+	cout << "You have enough space." << endl;
 	return true;
 }
 
+bool CheckMemory(const DWORDLONG physicalRAMNeeded, const DWORDLONG virtualRAMNeeded) {
+	MEMORYSTATUSEX status;
+	status.dwLength = sizeof(status);
+	GlobalMemoryStatusEx(&status);
+	if (status.ullTotalPhys < physicalRAMNeeded) {
+
+		cout << ("CheckMemory Failure: Not enough physical memory.");
+		return false;
+	}
+
+	if (status.ullAvailVirtual < virtualRAMNeeded) {
+
+		cout << ("CheckMemory Failure: Not enough virtual memory.");
+		return false;
+	}
+	char *buff = new char[virtualRAMNeeded];
+	if (buff)
+		delete[] buff;
+
+	cout << "Ram is available." << endl;
+	cout << "You have this much RAM: " << status.ullTotalPhys << endl;
+	cout << "You have this much vRAM: " << status.ullAvailVirtual << endl;
+	return true;
+}
+
+DWORD ReadCPUSpeed() {
+	DWORD BufSize = sizeof(DWORD);
+	DWORD dwMHz;
+	DWORD type = REG_DWORD;
+	HKEY hKey;
+	long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+		"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
+		0,
+		KEY_READ,
+		&hKey);
+	if (lError == ERROR_SUCCESS) {
+		RegQueryValueEx(hKey,
+			"MHz",
+			NULL,
+			&type,
+			(LPBYTE)&dwMHz,
+			&BufSize);
+
+	}
+	cout << "CPU in Hertz: " << dwMHz << endl;
+	system("pause");
+	return dwMHz;
+}
+
+
+int main() {
+
+	CheckInstance();
+
+	CheckStorage(300 * 1024 * 1024);
+
+	CheckMemory(69, 420);
+
+	ReadCPUSpeed();
+
+	return 0;
+}
