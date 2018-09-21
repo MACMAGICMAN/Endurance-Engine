b17a29a7d03fa36683e87bcd55fc4c5efd5a9fd1
BombardierBrisk <aljonjake45@gmail.com>
2018-09-21 12:59:54 -0400

    Half of Check Storage

--- EnduranceEngine/EnduranceEngine/MainSource.cpp
+++ EnduranceEngine/EnduranceEngine/MainSource.cpp
@@ -3,6 +3,10 @@
 #include <DirectXPackedVector.h>
 #include <iostream>
 #include <string>
+#include <stdio.h>  
+#include <direct.h>  
+#include <stdlib.h>  
+#include <ctype.h>  
 
 using namespace std;
 using namespace DirectX;
@@ -23,7 +27,28 @@ int main() {
 
 	}
 
+
+
 	cout << "asda";
 	system("pause");
 
 }
+
+bool CheckStorage(const DWORDLONG diskSpaceNeeded)
+{
+	int const drive = _getdrive();
+	struct _diskfree_t diskfree;
+
+	_getdiskfree(drive, &diskfree);
+
+	unsigned __int64 const neededClusters = diskSpaceNeeded / (diskfree.sectors_per_cluster*diskfree.bytes_per_sector);
+
+	if (diskfree.avail_clusters < neededClusters)
+	{
+		cout << "check";
+		return false;
+
+	}
+	return true;
+}
+
